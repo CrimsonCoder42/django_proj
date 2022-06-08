6abc2921b07382dc0f05375ecd1c3d7c90af6fd5
CrimsonCoder42 <53884312+CrimsonCoder42@users.noreply.github.com>
2022-06-08 11:25:07 -0400

    Update settings.py
    secret key removed

--- first_project/settings.py
+++ first_project/settings.py
@@ -22,7 +22,7 @@ STATIC_DIR = os.path.join(BASE_DIR, "static")
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w(%18u@1n2coeg777b^n*xs()%q&@z$e_(puewngp5x6$s&=yw'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
